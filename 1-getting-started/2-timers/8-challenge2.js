3990d6ccfbc26de7e090f71635709f2cb5a9431e
Brandon Pirrocco <bpirrocco@gmail.com>
2023-05-13 12:54:48 -0400

    Challenge #2

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,14 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+let count = 0
+
+const intervalId = setInterval(() => {
+    console.log("Hello World");
+    count += 1;
+
+    if (count === 5) {
+        console.log("Limit reached");
+        clearInterval(intervalId);
+    }
+}, 1000);
